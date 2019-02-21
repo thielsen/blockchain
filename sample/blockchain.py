0adfb779e0cd3441d8a1851da0e2c200925cf8a8
Simon Green <simon.green@thielsen.co.uk>
2019-02-21 07:59:28 +0000

    Test and pass for default addition

--- sample/blockchain.py
+++ sample/blockchain.py
@@ -1,8 +1,8 @@
 
-blockchain = [[1]]
+blockchain = [[]]
 
-def add_value(transaction_amount):
-    blockchain.append([get_last_blockchain_value(), transaction_amount])
+def add_value(transaction_amount, last_transaction=[1]):
+    blockchain.append([last_transaction, transaction_amount])
 
 def get_last_blockchain_value():
     return blockchain[-1]
--- tests/test_blockchain.py
+++ tests/test_blockchain.py
@@ -1,6 +1,10 @@
 from sample.blockchain import *
 
 def test_add_value_get_last_blockchain_value():
-    blockchain = [[1]]
-    add_value(5)
-    assert get_last_blockchain_value() == [[1], 5]
+    add_value(1)
+    add_value(5, get_last_blockchain_value())
+    assert get_last_blockchain_value() == [[[1], 1], 5]
+
+def test_add_default():
+    add_value(6)
+    assert get_last_blockchain_value() == [[1], 6]     
