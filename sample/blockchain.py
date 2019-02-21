fc61445e7d873c1c2e2290dd1b5acd4bc0cf4bd0
Simon Green <simon.green@thielsen.co.uk>
2019-02-21 19:03:41 +0000

    Refactor get_balance

--- sample/blockchain.py
+++ sample/blockchain.py
@@ -62,9 +62,9 @@ class BlockChain():
         tx_sender = [[tx['amount'] for tx in block['transactions'] if tx['sender'] == participant] for block in self.blockchain]
         open_tx_sender = [tx['amount'] for tx in self.open_transactions if tx['sender'] == participant]
         tx_sender.append(open_tx_sender)
-        amount_sent = functools.reduce(lambda x, y: x+y, functools.reduce(lambda x, y: x+y, tx_sender), 0)
+        amount_sent = functools.reduce(lambda x, y: x+sum(y), tx_sender, 0)
         tx_recipient = [[tx['amount'] for tx in block['transactions'] if tx['recipient'] == participant] for block in self.blockchain]
-        amount_received = functools.reduce(lambda x, y: x+y, functools.reduce(lambda x, y: x+y, tx_recipient), 0)
+        amount_received = functools.reduce(lambda x, y: x+sum(y), tx_recipient, 0)
         return amount_received - amount_sent
 
     def verify_transaction(self, transaction):
--- test.py
+++ test.py
@@ -3,9 +3,9 @@ import functools
 testblock = [{'index': 0, 'previous_hash': '', 'transactions': []}, {'index': 1, 'previous_hash': '0--[]', 'transactions': [{'amount': 10, 'recipient': 'Simon', 'sender': 'MINED'}]}, {'index': 2, 'previous_hash': "1-0--[]-[{'amount': 10, 'recipient': 'Simon', 'sender': 'MINED'}]", 'transactions': [{'amount': 3.4, 'recipient': 'Bob', 'sender': 'Simon'}, {'amount': 3.6, 'recipient': 'Alice', 'sender': 'Simon'}, {'amount': 10, 'recipient': 'Simon', 'sender': 'MINED'}]}]
 tx_sender = [[tx['amount'] for tx in block['transactions'] if tx['sender'] == 'Simon'] for block in testblock]
 
-output = functools.reduce(lambda x, y: x+y, functools.reduce(lambda x, y: x+y, tx_sender), 0)
+output = functools.reduce(lambda x, y: x+sum(y), tx_sender, 0)
 # output = functools.reduce(lambda x, y: x+y, tx_sender)
 # amount_sent = reduce(lambda tx_sum, tx_amt: tx_sum + reduce(lambda x, y: x+y, tx_amt) if len(tx_amt) > 0 else 0, tx_sender, 0)
-
-# print(tx_sender)
+output = sum(tx_sender)
+print(tx_sender)
 print(output)
