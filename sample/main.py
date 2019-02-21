69eb71be8d7e3f48b1b02b0e3757f2b7c5cea83c
Simon Green <simon.green@thielsen.co.uk>
2019-02-21 08:57:16 +0000

    Updated ui

--- sample/main.py
+++ sample/main.py
@@ -5,13 +5,16 @@ from blockchain import blockchain
 def get_user_input():
     return float(input('Transaction amount:'))
 
+
+
 tx_amount = get_user_input()
 add_value (tx_amount)
 
-tx_amount = get_user_input()
-add_value (tx_amount, get_last_blockchain_value())
 
-tx_amount = get_user_input()
-add_value (tx_amount, get_last_blockchain_value())
-
-print(blockchain)
+while True:
+    tx_amount = get_user_input()
+    add_value (tx_amount, get_last_blockchain_value())
+    for block in blockchain:
+        print('Outputting Block')
+        print(block)
+print('Done')
