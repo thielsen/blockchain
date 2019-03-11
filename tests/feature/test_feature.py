93c4151319f26ba958e88fb2c5f76722b0823ebf
Simon Green <simon.green@thielsen.co.uk>
2019-03-11 09:39:50 +0000

    Added test assertions

--- tests/feature/test_feature.py
+++ tests/feature/test_feature.py
@@ -20,47 +20,43 @@ def test_home_page(test_client):
 
 def test_blockchain(test_client):
     response = test_client.get("/blockchain")
-    print(response.data)
+    assert b"index" in response.data
     assert response.status_code == 200
 
 
 def test_mine(test_client):
     response = test_client.post("/mine")
-    print(response.data)
-    # add assert for json with test data
+    assert b"MINED" in response.data
     assert response.status_code == 201
 
 
 def test_wallet_post(test_client):
     response = test_client.post("/wallet")
-    print(response.data)
-    # add assert for json with test data
+    assert b"private_key" in response.data
     assert response.status_code == 201
 
 
 def test_wallet_get(test_client):
     response = test_client.get("/wallet")
     print(response.data)
-    # add assert for json with test data
+    assert b"private_key" in response.data
     assert response.status_code == 201
 
 
 def test_balance_get(test_client):
     response = test_client.get("/balance")
     print(response.data)
-    # add assert for json with test data
+    assert b"balance" in response.data
     assert response.status_code == 200
 
 
 def test_transaction_post(test_client):
     response = test_client.post("/transaction")
-    print(response.data)
-    # add assert for json with test data
+    assert b"No data provided" in response.data
     assert response.status_code == 400
 
 
 def test_transaction_get(test_client):
     response = test_client.get("/transactions")
-    print(response.data)
-    # add assert for json with test data
+    assert b"[]" in response.data
     assert response.status_code == 200
